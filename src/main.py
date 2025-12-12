12dfb99c1b0a7bc84931314d15a818b06fc48220
FaricJure <jure.faric@gmail.com>
2025-12-12 18:29:29 +0100

    TextNode Tests

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from textnode import TextNode, TextType
 
 def main():
-    print(TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev"))
-    
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
+
 if __name__ == "__main__":
     main()
