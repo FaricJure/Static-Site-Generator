ff316bd200c143e738a571959efde6ef86c9ba3c
FaricJure <jure.faric@gmail.com>
2025-12-13 21:41:27 +0100

    ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -35,3 +35,16 @@ class LeafNode(HTMLNode):
     
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag= None, children=None, props=None):
+        super().__init__(tag=tag, value=None, children=children, props=props)
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag to convert to HTML.")
+        
+        if self.children is None:
+            raise ValueError("ParentNode must have children to convert to HTML.")
+        
+        return f"<{self.tag}{self.props_to_html()}>" + "".join(child.to_html() for child in self.children) + f"</{self.tag}>"
--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
 from textnode import TextNode, TextType
-from htmlnode import LeafNode
+from htmlnode import LeafNode, ParentNode
 
 def main():
     node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     html_node = LeafNode("a", node.text, {"href": node.url})
-    print(html_node.to_html())
+    parent_node = ParentNode(
+    "p",
+    [
+        LeafNode("b", "Bold text"),
+        LeafNode(None, "Normal text"),
+        LeafNode("i", "italic text"),
+        LeafNode(None, "Normal text"),
+    ],
+)
+    print(parent_node.to_html())
 
 if __name__ == "__main__":
     main()
