eb91e8efff6be4c68bd6278af3304b5090e98220
FaricJure <jure.faric@gmail.com>
2025-12-17 18:45:37 +0100

    Deploy to the Internet

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 from extract_markdown import extract_title
 from markdown_to_html import markdown_to_html_node
@@ -16,7 +17,7 @@ def _remove_recursive(path: str) -> None:
         os.remove(path)
 
 
-def clear_directory(dst: str = "public", src: str = "static") -> None:
+def clear_directory(dst: str = "docs", src: str = "static") -> None:
     if os.path.exists(dst):
         print(f"Clearing directory: {dst}")
         _remove_recursive(dst)
@@ -27,21 +28,8 @@ def clear_directory(dst: str = "public", src: str = "static") -> None:
     os.makedirs(dst, exist_ok=True)
     print(f"Copying static files to: {dst}")
     shutil.copytree(src, dst, dirs_exist_ok=True)
-    print("Writing the new full HTML page to the destination directory.")
-    generate_pages_recursive(
-        dir_path_content="content",
-        template_path="template.html",
-        dest_dir_path="public",
-    )
-    """
-    generate_page(
-        from_path="content/index.md",
-        template_path="template.html",
-        dest_path="public/index.html",
-    )
-    """
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r", encoding="utf-8") as f:
         from_path_content = f.read()
@@ -54,23 +42,35 @@ def generate_page(from_path, template_path, dest_path):
     page_title = extract_title(from_path_content)[0]
     final_page = template_content.replace("{{ Content }}", final_content).replace("{{ Title }}", page_title)
 
+    final_page = final_page.replace("href=\"/\"", f'href="{basepath}"')
+    final_page = final_page.replace("src=\"/\"", f'src="{basepath}"')
+
     with open(dest_path, "w", encoding="utf-8") as f:
         f.write(final_page)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for entry in os.scandir(dir_path_content):
         if entry.is_file() and entry.name.endswith(".md"):
             relative_path = os.path.relpath(entry.path, dir_path_content)
             dest_path = os.path.join(dest_dir_path, relative_path[:-3] + ".html")
             os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-            generate_page(entry.path, template_path, dest_path)
+            generate_page(entry.path, template_path, dest_path, basepath)
+
         elif entry.is_dir():
             new_dest_dir = os.path.join(dest_dir_path, entry.name)
             os.makedirs(new_dest_dir, exist_ok=True)
-            generate_pages_recursive(entry.path, template_path, new_dest_dir)
+            generate_pages_recursive(entry.path, template_path, new_dest_dir, basepath)
 
 def main():
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
     clear_directory()
+    print("Writing the new full HTML page to the destination directory.")
+    generate_pages_recursive(
+        dir_path_content="content",
+        template_path="template.html",
+        dest_dir_path="docs",
+        basepath=basepath,
+    )
 
 
 if __name__ == "__main__":
